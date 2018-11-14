630534380075640f80c54a3e11c88ada15b55f26
irezwi <zwirek.ireneusz@gmail.com>
2018-11-14 20:01:40 +0100

    Automate getting user local IP

--- app.py
+++ app.py
@@ -2,10 +2,20 @@ import os
 import re
 
 # Dict with addresses to find
-persons = {'AA:BB:CC:DD:EE:FF': 'John Doe'}
+persons = {
+    'AA:BB:CC:DD:EE:FF': 'John Doe',
+}
+
+# Get user local IP address
+hostname_response = os.popen('hostname -I').read().split('.')
+hostname_response[-1] = '0'
+hostname_response = '.'.join(hostname_response)
+
+# Remove empty lines and spaces
+user_local_ip = "".join(hostname_response).replace('\n', '').replace(' ', '')
 
 # Searching for MAC addresses in arp-scan request
-arp_out = " ".join(os.popen('sudo arp-scan 192.168.1.0/24 -g').readlines())
+arp_out = " ".join(os.popen('sudo -S arp-scan ' + user_local_ip + '/25 ' + '-g').readlines())
 mac_addresses = re.findall(r'(?:[0-9a-fA-F]:?){12}', arp_out)
 
 # Printing found addresses
